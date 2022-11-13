a851268619b0a89c2ff3af7cb2ecaa54b4f9fffb
Morgan Haworth <morgan123haworth@gmail.com>
2022-11-13 07:48:10 -0600

    FIx npm structure

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -3,12 +3,13 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import App from "./App.vue";
-import router from "./router";
 
 import "./assets/main.css";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 
